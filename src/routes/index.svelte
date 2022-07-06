3be282ae7b45d43830f82e9ff4ffbd9085ebbb5f
CREEK <brandon@gridviewdesign.com>
2022-07-05 20:48:07 -0500

    init

---
+++ src/routes/index.svelte
@@ -0,0 +1,27 @@
+<script>
+	import About from '$lib/components/About.svelte';
+	import Feature from '$lib/components/Feature.svelte';
+	import Hero from '$lib/components/Hero.svelte';
+	import Section from '$lib/components/Section.svelte';
+	import Team from '$lib/components/Team.svelte';
+	import Faq from '$lib/components/Faq.svelte';
+	import Footer from '$lib/components/Footer.svelte';
+</script>
+
+<Hero
+	brand="Greetings Frens"
+	title="Celebrate the Special Moments"
+	content="With Greetings Frens, you can send someone you  love a birthday card that's unique, one-of-a-kind, and powered by NFTs. Whether it's your best friend's birthday, your mom's retirement, or your child's graduation, Greetings Frens let you celebrate the people who matter most in your life in a truly special way."
+/>
+
+<About />
+
+<Section />
+
+<Feature />
+
+<Team />
+
+<Faq />
+
+<Footer />
